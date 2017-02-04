2ddb3325f43bb2c579c951104deece808f7aa324
weaponhe <weaponhe@126.com>
2017-02-04 11:18:22 +0800

    04

---
+++ 04/webpack.config.js
@@ -0,0 +1,20 @@
+var webpack    = require('webpack')
+module.exports = function ()
+{
+  return {
+    entry: {
+      main: './src/index.js',
+      vendor: 'moment'
+    },
+    output: {
+      filename: '[name].js',
+      path: './dist'
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
+      new webpack.optimize.UglifyJsPlugin({})
+    ]
+  }
+}
